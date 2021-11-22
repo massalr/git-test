7e3094146b2a971f61aec00b47332fe661621db7
massalr <lookingforyourname@gmail.com>
2021-11-22 09:39:24 +0700

    madlibs mini ke 1

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+"""
+terdapat beberapa string kosong yang akan di gabungkan menjadi sebuah kalimat,
+string tersebut di isi menggunakan variable yang sudah di buat.
+"""
+putri_raja = input("putri raja: ")
+pangeran = input("pangeran: ")
+candi = input("candi: ")
+jumlah = input("jumlah: ")
+
+madlib = f"disebuah tempat seorang pangerang {pangeran} tergila2 dengan seorang putri bernama {putri_raja} \
+    namun putri raja memberikan syarat untuk membuat {candi} sebanyak {jumlah}"
+
+print("*"*100)
+print(madlib)
